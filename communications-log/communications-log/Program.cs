b27e461f9c7b417123024bba8b58e0080f3bc4a7
David Jennings <davidj0525@yahoo.com>
2021-06-22 09:20:28 -0700

    Go Chargers

--- communications-log/communications-log/Program.cs
+++ communications-log/communications-log/Program.cs
@@ -6,6 +6,11 @@ namespace communications_log
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
             Console.WriteLine("Hello Pilot!");
 
             Console.WriteLine("This is ground control to Major Tom");
